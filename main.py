621bf4de2edc904958e626c8b32d8b87b1758976
Arno Van Eetvelde <arno@Arnos-MacBook-Pro.local>
2023-07-05 16:58:04 +0200

    main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ os.environ["OPENAI_API_KEY"] = api_key.APIKEY
 
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.chat_models import ChatOpenAI
 
 query = sys.argv[1]
 
@@ -13,4 +14,4 @@ loader = TextLoader('data.txt')
 
 index = VectorstoreIndexCreator().from_loaders([loader])
 
-print(index.query(query))
+print(index.query(query, llm=ChatOpenAI))
